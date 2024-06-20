7f719622d1628ac6f76f84f9daca96f24444375c
hiroki.kinjo <hiroki-kinjo@dac.co.jp>
2024-06-20 00:37:45 +0000

    save 6/20

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -19,7 +19,23 @@
                             {{-- 投稿内容 --}}
                             <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                         </div>
-                        <div>
+                        <div class = "flex">
+                            @if (Auth::user()->is_favoriting($micropost->id))
+                                    {{-- unfavoriteボタンのフォーム --}}
+                                    <form method="POST" action="{{ route('favor.unfavorite', $micropost->id) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-error btn-sm normal-case" 
+                                        onclick="return confirm('id = {{ $micropost->id }} のお気に入りを外します。よろしいですか？')">Unfavorite</button>
+                                    </form>
+                            @else
+                                    {{-- favoriteボタンのフォーム --}}
+                                    <form method="POST" action="{{ route('favor.favorite', $micropost->id) }}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary btn-sm normal-case">Favorite</button>
+                                    </form>
+                            @endif
+                            
                             @if (Auth::id() == $micropost->user_id)
                                 {{-- 投稿削除ボタンのフォーム --}}
                                 <form method="POST" action="{{ route('microposts.destroy', $micropost->id) }}">
@@ -28,23 +44,6 @@
                                     <button type="submit" class="btn btn-error btn-sm normal-case" 
                                         onclick="return confirm('Delete id = {{ $micropost->id }} ?')">Delete</button>
                                 </form>
-                                
-                            @else
-                                @if (Auth::user()->is_favoriting($micropost->id))
-                                    {{-- アンフォローボタンのフォーム --}}
-                                    <form method="POST" action="{{ route('favor.unfavorite', $micropost->id) }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-error btn-sm normal-case" 
-                                        onclick="return confirm('id = {{ $micropost->id }} のお気に入りを外します。よろしいですか？')">Unfavorite</button>
-                                    </form>
-                                @else
-                                    {{-- フォローボタンのフォーム --}}
-                                    <form method="POST" action="{{ route('favor.favorite', $micropost->id) }}">
-                                        @csrf
-                                        <button type="submit" class="btn btn-primary btn-sm normal-case">Favorite</button>
-                                    </form>
-                                @endif
                             @endif
                         </div>
                     </div>
